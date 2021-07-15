8f4f21a2d4ac5f953de174bdcc748f03eddf094e
waghprashant8128 <waghprashant8128@gmail.com>
2021-07-15 20:59:37 +0530

    1

---
+++ pro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int n;
+  printf("Enter a Number to Check Even or Odd\n");
+  scanf("%d", &n);
+  if (n%2 == 0)
+    printf("The Number is Even\n");
+  else
+    printf("The Number is Odd\n");
+  return 0;
+}
